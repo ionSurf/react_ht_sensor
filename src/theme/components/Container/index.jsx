6715f26eebe1ae6cf69671f621b05be16b987110
Javier Prieto <jprieto@orangerine.com>
2022-02-27 19:41:31 -0500

    Added MUI support for Container theme component

--- src/theme/components/Container/index.jsx
+++ src/theme/components/Container/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import propTypes from "prop-types";
+import { Container as MuiContainer } from "@mui/material";
 
-const Container = ({ classes = "", children, ...props }) => (
-  <div className={classes} {...props}>
+const Container = ({ classes = "", children, maxWidth = "sm", ...props }) => (
+  <MuiContainer maxWidth={maxWidth} className={classes} {...props}>
     {children}
-  </div>
+  </MuiContainer>
 );
 Container.propTypes = {
   classes: propTypes.string,
+  maxWidth: propTypes.string,
   children: propTypes.oneOfType([
     propTypes.object,
     propTypes.arrayOf(propTypes.object),
