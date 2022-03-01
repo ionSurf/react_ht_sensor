7783fe349c544a1ecf9cd142fa0cfd0f25075f91
Javier Prieto <jprieto@orangerine.com>
2022-03-01 07:41:54 -0500

    Exported missing componnents in theme's barrel index

--- src/theme/components/index.js
+++ src/theme/components/index.js
@@ -1,6 +1,8 @@
+import Box from "./Box";
+import Grid from "./Grid";
 import Link from "./Link";
 import Image from "./Image";
 import Button from "./Button";
 import Container from "./Container";
 
-export { Image, Link, Button, Container };
+export { Box, Grid, Image, Link, Button, Container };
