f1c89a1d3580fa5fc0c4a949aef1713b5fe4405b
Javier Prieto <jprieto@orangerine.com>
2022-03-01 21:52:46 -0500

    Updated SectionHeading to use theme's Heading component

--- src/pages/SensorSettingsPage/Layout/components/SectionHeading.jsx
+++ src/pages/SensorSettingsPage/Layout/components/SectionHeading.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import propTypes from "prop-types";
+import { Heading } from "../../../../theme/textStyles/";
 
 export default function SectionHeading({ children }) {
-  return <>{children}</>;
+  return <Heading typography="h2">{children}</Heading>;
 }
 SectionHeading.propTypes = {
   children: propTypes.oneOfType([
