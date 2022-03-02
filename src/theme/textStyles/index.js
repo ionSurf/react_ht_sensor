c791506e1abc5535aea49ff6d727c0eb5b1f18a0
Javier Prieto <jprieto@orangerine.com>
2022-03-01 21:51:06 -0500

    Fixed barrel index for textStyles in theme

---
+++ src/theme/textStyles/index.js
@@ -0,0 +1,7 @@
+import BodyText from "./BodyText";
+import ButtonText from "./ButtonText";
+import Caption from "./Caption";
+import Heading from "./Heading";
+import Subtitle from "./Subtitle";
+
+export { BodyText, ButtonText, Caption, Heading, Subtitle };
