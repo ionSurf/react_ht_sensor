0fedf832c229f229a31c4cebed66748788290567
Javier Prieto <jprieto@orangerine.com>
2022-03-01 19:44:22 -0500

    Relocated Layout subcomponents

--- src/pages/SensorSettingsPage/components/layout/index.js
+++
@@ -1,8 +0,0 @@
-import Row from "./Row";
-import Column from "./Column";
-import Button from "./Button";
-import Heading from "./Heading";
-import TextField from "./TextField";
-import ToggleButton from "./ToggleButton";
-
-export { Row, Column, Button, Heading, TextField, ToggleButton };
