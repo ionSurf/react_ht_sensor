cfa15128d2d6129297ccbe21d807c8a610129878
Javier Prieto <jprieto@orangerine.com>
2022-03-01 21:52:05 -0500

    Added TextInputField component to theme

---
+++ src/theme/components/TextInputField/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+const TextInputField = (props) => <TextField {...props} />;
+export default TextInputField;
--- src/theme/components/index.js
+++ src/theme/components/index.js
@@ -4,5 +4,6 @@ import Link from "./Link";
 import Image from "./Image";
 import Button from "./Button";
 import Container from "./Container";
+import TextInputField from "./TextInputField";
 
-export { Box, Grid, Image, Link, Button, Container };
+export { Box, Grid, Image, Link, Button, Container, TextInputField };
