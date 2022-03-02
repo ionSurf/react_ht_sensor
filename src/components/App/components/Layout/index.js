b72f6f7d4ed9523cb3da5c53278b83318a658344
Javier Prieto <jprieto@orangerine.com>
2022-03-01 19:32:55 -0500

    Relocated and refactored Layout component and subcomponents

--- src/components/App/components/Layout/index.js
+++ src/components/App/components/Layout/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import propTypes from "prop-types";
 import { Head, Body } from "./components";
 import { Container, Box } from "../../../../theme/components";
+// import { useTheme } from "@mui/material";
 
-const Layout = ({ children }) => (
-  <Container disableGutters maxWidth={"false"}>
-    <Box>{children}</Box>
-  </Container>
-);
+const Layout = ({ children }) => {
+  // const theme = useTheme();
+  return (
+    <Container
+      // sx={{
+      //   backgroundColor: "background.default",
+      // }}
+      disableGutters
+      maxWidth={"false"}
+    >
+      <Box>{children}</Box>
+    </Container>
+  );
+};
 Layout.propTypes = {
   children: propTypes.arrayOf(propTypes.object),
 };
