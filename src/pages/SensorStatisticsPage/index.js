a881ccc67f806d718dd939967d231e32d3395868
Javier Prieto <jprieto@orangerine.com>
2022-03-03 22:07:56 -0500

    Added SensorStatisticsPage shell

--- src/pages/SensorStatisticsPage/index.js
+++ src/pages/SensorStatisticsPage/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import SensorStatisticsPageView from "./view";
 
 const SensorStatisticsPage = () => {
-  return <SensorStatisticsPageView>Statistics</SensorStatisticsPageView>;
+  const [_samples_set, set_samples_set] = useState({
+    hum: [],
+    tmp: [],
+  });
+  const _statistics = {
+    hum: {
+      min: (_samples_set.hum.length > 0 && Math.min(_samples_set.hum)) || 0,
+      max: Math.max(_samples_set.hum) ?? 0,
+      avg:
+        (_samples_set.hum.length > 0 &&
+          _samples_set.hum.reduce((p, c) => p + c, 0)) ||
+        0,
+    },
+    tmp: {
+      min: Math.min(_samples_set.tmp) ?? 0,
+      max: Math.max(_samples_set.tmp) ?? 0,
+      avg:
+        (_samples_set.tmp.length > 0 &&
+          _samples_set.tmp.reduce((p, c) => p + c, 0)) ||
+        0,
+    },
+  };
+  set_samples_set;
+  return (
+    <SensorStatisticsPageView samples={_samples_set} stats={_statistics} />
+  );
 };
 export default SensorStatisticsPage;
