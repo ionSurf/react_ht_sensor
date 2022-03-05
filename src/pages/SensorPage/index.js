8fbc19bd9d9d9de8895f6e420c8595ca906cc116
Javier Prieto <jprieto@orangerine.com>
2022-03-04 23:09:29 -0500

    Integrated WebSocketProvider into SensorPage

--- src/pages/SensorPage/index.js
+++ src/pages/SensorPage/index.js
@@ -3,6 +3,8 @@ import SensorPageView from "./view";
 import { SensorTabs } from "./components";
 import Tabs from "../../components/Tabs";
 
+import useWebSocketContext from "../../hooks/useWebSocketContext";
+
 //  Pages
 import {
   SensorSamplingPage,
@@ -11,51 +13,54 @@ import {
 } from "../";
 
 const SensorPage = () => {
+  const { provider: WebSocketProvider } = useWebSocketContext;
   return (
-    <SensorPageView>
-      <SensorTabs
-        tabs={[
-          {
-            id: "sampling",
-            name: "Sampling",
-          },
-          {
-            id: "statistics",
-            name: "Statistics",
-          },
-          {
-            id: "settings",
-            name: "Settings",
-          },
-        ]}
-      >
-        {(activeTabId) => (
-          <>
-            <Tabs.Tab
-              activeTabId={activeTabId}
-              id={"sampling"}
-              // title={"Sampling"}
-            >
-              <SensorSamplingPage />
-            </Tabs.Tab>
-            <Tabs.Tab
-              activeTabId={activeTabId}
-              id={"statistics"}
-              // title={"Statistics"}
-            >
-              <SensorStatisticsPage />
-            </Tabs.Tab>
-            <Tabs.Tab
-              activeTabId={activeTabId}
-              id={"settings"}
-              // title={"Statistics"}
-            >
-              <SensorSettingsPage />
-            </Tabs.Tab>
-          </>
-        )}
-      </SensorTabs>
-    </SensorPageView>
+    <WebSocketProvider value={{ endpoint: "ws://localhost:8888/ws" }}>
+      <SensorPageView>
+        <SensorTabs
+          tabs={[
+            {
+              id: "sampling",
+              name: "Sampling",
+            },
+            {
+              id: "statistics",
+              name: "Statistics",
+            },
+            {
+              id: "settings",
+              name: "Settings",
+            },
+          ]}
+        >
+          {(activeTabId) => (
+            <>
+              <Tabs.Tab
+                activeTabId={activeTabId}
+                id={"sampling"}
+                // title={"Sampling"}
+              >
+                <SensorSamplingPage />
+              </Tabs.Tab>
+              <Tabs.Tab
+                activeTabId={activeTabId}
+                id={"statistics"}
+                // title={"Statistics"}
+              >
+                <SensorStatisticsPage />
+              </Tabs.Tab>
+              <Tabs.Tab
+                activeTabId={activeTabId}
+                id={"settings"}
+                // title={"Statistics"}
+              >
+                <SensorSettingsPage />
+              </Tabs.Tab>
+            </>
+          )}
+        </SensorTabs>
+      </SensorPageView>
+    </WebSocketProvider>
   );
 };
 export default SensorPage;
